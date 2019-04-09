c40730480d25d174fe1ef09db2742e744c5b98dc
Mihajlo Kusljic <mihajlokusljic97@gmail.com>
2019-04-09 03:30:38 +0200

    Stilizovane ikonice

--- WeatherForecast/WeatherForecast/obj/Debug/WeatherForecast_Content.g.i.cs
+++ WeatherForecast/WeatherForecast/obj/Debug/WeatherForecast_Content.g.i.cs
@@ -8,7 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("resources/images/checkstar_hover.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("resources/images/checkstar_on.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("resources/images/weathericon.png")]
 
 
